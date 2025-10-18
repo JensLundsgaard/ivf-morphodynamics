24d2397f90536c279ac8a124599f97291b061ccb
Jens Lundsgaard <jenslundsgaard7@gmail.com>
2025-10-18 16:36:35 +0000

    messing with vim

--- encoder.py
+++ encoder.py
@@ -1,5 +1,7 @@
 import torch
 import os
+
+from torchsummary import summary
 #from tphate import tphate
 class Model(torch.nn.Module):
     def __init__(self):
@@ -54,6 +56,36 @@ class Model(torch.nn.Module):
 model = Model()
 if os.path.exists("model_weights.pth"):
     model.load_state_dict(torch.load("model_weights.pth",weights_only = True))
+"""
+ignore this just me having fun with vim, but still has a use case for grabbing output like torchsummary of a model
+
+:let @a = system("python3 encoder.py") 
+"ap
+
+----------------------------------------------------------------
+        Layer (type)               Output Shape         Param #
+================================================================
+            Conv2d-1          [-1, 8, 498, 498]              80
+         MaxPool2d-2          [-1, 8, 166, 166]               0
+            Conv2d-3          [-1, 8, 164, 164]             584
+         MaxPool2d-4            [-1, 8, 32, 32]               0
+            Conv2d-5            [-1, 8, 28, 28]           1,608
+         MaxPool2d-6              [-1, 8, 5, 5]               0
+           Flatten-7                  [-1, 200]               0
+            Linear-8                  [-1, 200]          40,200
+           Sigmoid-9                  [-1, 200]               0
+================================================================
+Total params: 42,472
+Trainable params: 42,472
+Non-trainable params: 0
+----------------------------------------------------------------
+Input size (MB): 0.95
+Forward/backward pass size (MB): 18.58
+Params size (MB): 0.16
+Estimated Total Size (MB): 19.69
+----------------------------------------------------------------
+None
+"""
 
 class Enc_Model(torch.nn.Module):
     def __init__(self,model = Model()):
@@ -67,7 +99,7 @@ class Enc_Model(torch.nn.Module):
         self.flatten = model.flatten
         self.linear = model.linear
         self.activation = model.activation
-    def forward(x):
+    def forward(self,x):
         x = self.conv1(x)
         x = self.pool1(x)
         x = self.conv2(x)
@@ -79,5 +111,7 @@ class Enc_Model(torch.nn.Module):
         x = self.activation(x)
         return x
 enc_model = Enc_Model(model = model)
+print(summary(enc_model, input_size = (1,500,500), batch_size = -1))
+
 #tphate_op = tphate.TPHATE()
 #data_tphate = tphate_op.fit_transform(data)
