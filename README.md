bc8d3121a1ea2d32da12167b10d75ae7926397db
Jens Lundsgaard <jenslundsgaard7@gmail.com>
2025-10-18 16:19:58 +0000

    started working on getting tphate deps installed and worked on readme

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 This readme is super WIP. Just writing some notes about dependencies and stuff
 Notes:
-    * If running on linux do "source .venv/bin/activate" and then install necessary dependencies. 
-    * If running on mac/windows just install all the necessary dependencies with pip install
-    * If you clone to linux you'll need to create a .venv environment, run "source .venv/bin/activate", and install the dependencies there
-    * Current .venv dependencies: natsort, torch (PyTorch), tphate
-    * Most of the other dependencies are just installed globally nothing crazy, numpy and stuff
+* Make sure you have the data downloaded and in the following folders/ files: 
+    * embryo\_dataset 
+    * embryo\_dataset\_annotations
+    * embryo\_dataset/*  
+    * embryo\_dataset\_annotations/*
+    * embryo\_dataset\_grades.csv 
+* If running on mac/windows just install all the necessary dependencies with pip install
+* If you clone to linux you'll need to create a .venv environment, run "source .venv/bin/activate", and install the dependencies there
+* Current .venv dependencies: natsort, torch (PyTorch), tphate, scipy,pygsp,tasklogger,scikit-learn, future, statsmodels, scprep
+* Reminder: Get a Pipfile set up to do the above
+* Most of the other dependencies are just installed globally nothing crazy, numpy and stuff
+* (note so I don't forget) If you want to connect git to github (i.e. only have access to a command line), you create a ssh key, add the public key to github, and set up remotes for the repo as "git remote add origin git@github.com:JensLundsgaard/ivf.git". To find where git looks for the private key run "ssh -vT git@github.com"
---
+++ encoder.py
@@ -0,0 +1,83 @@
+import torch
+import os
+#from tphate import tphate
+class Model(torch.nn.Module):
+    def __init__(self):
+        super().__init__() # Call the constructor of the parent class
+        self.conv1 = torch.nn.Conv2d(1, 8, 3)
+        self.pool1 = torch.nn.MaxPool2d(3)
+        self.conv2 = torch.nn.Conv2d(8, 8, 3)
+        self.pool2 = torch.nn.MaxPool2d(5)
+        self.conv3 = torch.nn.Conv2d(8, 8, 5)
+        self.pool3 = torch.nn.MaxPool2d(5)
+        self.flatten = torch.nn.Flatten()
+        self.linear = torch.nn.Linear(200, 200)
+        self.lstm = torch.nn.LSTM(200,200,1)
+        self.unflatten = torch.nn.Unflatten(1,(8,5,5))
+        self.upsample1 = torch.nn.UpsamplingBilinear2d(scale_factor=2)
+        self.conv4 = torch.nn.Conv2d(8,16,3)
+        self.conv5 = torch.nn.Conv2d(16,16,3,padding = 1)
+        self.conv6 = torch.nn.Conv2d(16,16,3, padding = 1)
+        self.conv7 = torch.nn.Conv2d(16,8,3, padding = 1)
+        self.conv8 = torch.nn.Conv2d(8,8,3, padding = 1)
+        self.conv9 = torch.nn.Conv2d(8,4,3, padding = 1)
+        self.conv10 = torch.nn.Conv2d(4,1,13)
+        self.activation = torch.nn.Sigmoid()
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.pool1(x)
+        x = self.conv2(x)
+        x = self.pool2(x)
+        x = self.conv3(x)
+        x = self.pool3(x)
+        x = self.flatten(x)
+        x = self.linear(x)
+        x = self.activation(x)
+        x,(h,c) = self.lstm(x, (torch.zeros(1,200),torch.zeros(1,200))) 
+        x = self.unflatten(x)
+        x = self.upsample1(x)
+        x = self.conv4(x)
+        x = self.upsample1(x)
+        x = self.conv5(x)
+        x = self.upsample1(x)
+        x = self.conv6(x)
+        x = self.upsample1(x)
+        x = self.conv7(x)
+        x = self.upsample1(x)
+        x = self.conv8(x)
+        x = self.upsample1(x)
+        x = self.conv9(x)
+        x = self.upsample1(x)
+        x = self.conv10(x)
+        return x
+
+model = Model()
+if os.path.exists("model_weights.pth"):
+    model.load_state_dict(torch.load("model_weights.pth",weights_only = True))
+
+class Enc_Model(torch.nn.Module):
+    def __init__(self,model = Model()):
+        super().__init__() # Call the constructor of the parent class
+        self.conv1 = model.conv1
+        self.pool1 = model.pool1
+        self.conv2 = model.conv2
+        self.pool2 = model.pool2
+        self.conv3 = model.conv3
+        self.pool3 = model.pool3
+        self.flatten = model.flatten
+        self.linear = model.linear
+        self.activation = model.activation
+    def forward(x):
+        x = self.conv1(x)
+        x = self.pool1(x)
+        x = self.conv2(x)
+        x = self.pool2(x)
+        x = self.conv3(x)
+        x = self.pool3(x)
+        x = self.flatten(x)
+        x = self.linear(x)
+        x = self.activation(x)
+        return x
+enc_model = Enc_Model(model = model)
+#tphate_op = tphate.TPHATE()
+#data_tphate = tphate_op.fit_transform(data)
